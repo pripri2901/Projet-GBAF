3f068f7ae5dbd1bd49339bc50e94620eb0d47ee3
pripri2901 <pripri2901@gmail.com>
2021-06-11 21:28:59 +0200

    Début

--- mini-blog-php/espace-membre.php
+++ mini-blog-php/espace-membre.php
@@ -26,9 +26,23 @@ $articles = getArticles();
    
 
 
-   <div class="container-fluid">
+  
        <h1>Présentation du GBAF et du site </h1>
-
+       <div class="paragraphe">
+            <p> Le Groupement Banque Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français :
+</br>
+<lu>
+<li> BNP Paribas</li>
+<li>BPCE</li>
+<li>Crédt Agricole</li>
+<li>Crédit Mutuel-CIC</li>
+<li>Société Générale</li>
+<li>La Banque Postale</li>
+</lu></br>
+Même s’il existe une forte concurrence entre ces entités, elles vont toutes travailler de la même façon pour gérer près de 80 millions de comptes sur le territoire national.
+Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des pouvoirs publics.
+</p>
+    </div>
        <h2>Acteurs et partenaires</h2><br />
       
 
@@ -39,7 +53,7 @@ $articles = getArticles();
        <br /><br />
            <a href="article.php?id=<?= $article->id ?>">Lire la suite</a>
        <?php endforeach; ?>
-       </div>
+    
    
    
    <?php include('view/footer.php'); ?>
--- mini-blog-php/view/footer.php
+++ mini-blog-php/view/footer.php
@@ -1,3 +1,15 @@
-</body>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+</head>
+<body>
+    <div class="pieds">
+<a href="mentions-legales.html">mentions l'égales</a>
+<a href="contact.html">Contact</a>
+    </div>
+</body>
 </html>
